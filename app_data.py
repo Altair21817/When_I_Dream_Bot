b7b47ca7c84b071c7c7c8eb63665f6aac010ac68
Kirill Svidunovich <Altair21817@gmail.com>
2023-05-06 22:54:33 +0300

    Create data and settings apps

---
+++ app_data.py
@@ -0,0 +1,175 @@
+from dotenv import load_dotenv
+from pathlib import Path
+from telegram import InputMediaPhoto
+import os
+
+load_dotenv()
+
+TELEGRAM_BOT_TOKEN: str = os.getenv('TELEGRAM_BOT_TOKEN')
+ALL_DATA: list[str] = [TELEGRAM_BOT_TOKEN]
+
+# Вообще надо нарисовать карту возможных развитий событий тыканья кнопок
+BUTTON_ADD_ME_PENALTY: str = '/add_me_penalty'
+BUTTON_BEGIN: str = '/begin'
+BUTTON_CREATE: str = '/create'
+BUTTON_CORRECT_ANSWER: str = '/YES'
+BUTTON_EXIT: str = '/exit'
+BUTTON_HELP: str = '/help'
+BUTTON_INCORRECT_ANSWER: str = '/NO'
+BUTTON_JOIN: str = '/join'
+BUTTON_RULES: str = '/rules'
+BUTTON_START: str = '/start'
+BUTTON_START_NEXT_ROUND: str = '/next_round'
+
+IMAGE_CARDS_ORIGINAL_PATH: Path = Path('res/words_original')
+IMAGE_CARDS_ROTATED_PATH: Path = Path('res/words_rotated')
+IMAGE_CARDS: list[Path] = (
+    list(IMAGE_CARDS_ORIGINAL_PATH.iterdir())
+    + list(IMAGE_CARDS_ROTATED_PATH.iterdir()))
+IMAGE_RULES: list[Path] = [
+    Path(f'res/rules/0{i}_правила.jpg') for i in range(8)]
+IMAGE_RULES_MEDIA: list[InputMediaPhoto] = []
+for file_path in IMAGE_RULES:
+    with open(file_path, 'rb') as file:
+        IMAGE_RULES_MEDIA.append(InputMediaPhoto(
+            media=file.read(), caption=Path(file_path).name))
+
+KEYBOARD_EMPTY: list[list[str]] = [
+    ['']]
+KEYBOARD_IN_GAME: list[list[str]] = [
+    [BUTTON_CORRECT_ANSWER, BUTTON_ADD_ME_PENALTY, BUTTON_INCORRECT_ANSWER]]
+KEYBOARD_IN_LOBBY: list[list[str]] = [
+    [BUTTON_RULES, BUTTON_HELP, BUTTON_EXIT]]
+KEYBOARD_IN_LOBBY_HOST: list[list[str]] = [
+    [BUTTON_BEGIN, BUTTON_RULES, BUTTON_HELP, BUTTON_EXIT]]
+KEYBOARD_MAIN_MENU: list[list[str]] = [
+    [BUTTON_CREATE, BUTTON_JOIN, BUTTON_RULES, BUTTON_HELP]]
+KEYBOARD_START_NEXT_ROUND: list[list[str]] = [
+    [BUTTON_START_NEXT_ROUND, BUTTON_EXIT]]
+
+BUKA: str = 'buka'
+DREAMER: str = 'dreamer'
+FAIRY: str = 'fairy'
+SANDMAN: str = 'sandman'
+CHARACTERS_CONFIG: dict[int, dict] = {
+    3:  {BUKA: 1, FAIRY: 1, SANDMAN: 1},
+    4:  {BUKA: 1, FAIRY: 1, SANDMAN: 2},
+    5:  {BUKA: 1, FAIRY: 2, SANDMAN: 2},
+    6:  {BUKA: 2, FAIRY: 3, SANDMAN: 1},
+    7:  {BUKA: 2, FAIRY: 3, SANDMAN: 2},
+    8:  {BUKA: 3, FAIRY: 4, SANDMAN: 1},
+    9:  {BUKA: 3, FAIRY: 4, SANDMAN: 2},
+    10: {BUKA: 4, FAIRY: 5, SANDMAN: 1}}
+
+MESSAGE_CANT_CREATE_OR_JOIN: str = (
+    'Сновидец, я вижу, что ты уже участвуешь в одной из игр. Чтобы создать '
+    'новую или подключиться к другой, тебе сначала нужно выйти из текущей! '
+    f'Для этого воспользуйся командой {BUTTON_EXIT}.')
+MESSAGE_CREATE_GAME: str = (
+    'Приветствую, капитан! Ты готов отправиться со своей командой в новое '
+    'путешествие по миру снов? Отлично! Пожалуйста, пришли мне пять цифр, '
+    'которые будут являться паролем к игре!')
+MESSAGE_CREATE_GAME_FAILED: str = (
+    'Какая удача, капитан! Ты угадал чей-то пароль! А ведь шанс такого '
+    'события составляет менее 1%! Невероятно! Пожалуйста, пришли мне иную '
+    'комбинацию цифр, попробуем еще раз!')
+MESSAGE_CREATE_GAME_PASS: str = (
+    'Пароль принят, капитан! Теперь в точности сообщи его своим друзьям, '
+    f'чтобы они смогли подключиться через команду {BUTTON_JOIN}.''\n\n'
+    f'Когда вся команда будет в сборе, используй команду {BUTTON_BEGIN} для '
+    'начала игры. Желаю здорово повеселиться!\n\n'
+    f'А если вдруг ты решишь проснутся - используй команду {BUTTON_EXIT}. '
+    'Игра при этом не остановится!')
+MESSAGE_GAME_BEGIN: str = (
+    'Твое путешествие начинается через.. 3.. 2.. 1.. Сейчас!')
+MESSAGE_GREET_1: str = (
+    'Добро пожаловать, о чудесный сновидец!\n\n'
+    'Сегодня ты отправляешься в невероятное путешествие по миру снов. Миру, '
+    'где обитают сказочные создания, вершится магия и процветает фантазия. '
+    'Там ты встретишь друзей и врагов, правдолюбов и лжецов, людей искренних '
+    'и лицемерных.\n\n'
+    'Чтобы успешно пройти через любые испытания тебе нужно проявить смелость, '
+    'умение думать на ходу и, конечно же, чувство юмора. Чтобы помочь тебе на '
+    'этом пути я высылаю тебе правила игры. Приятных снов!')
+MESSAGE_GREET_2: str = (
+    f'Нажми {BUTTON_CREATE}, чтобы создать новую игру.''\n'
+    f'Нажми {BUTTON_JOIN}, чтобы присоединиться к игре.''\n'
+    f'Нажми {BUTTON_RULES}, чтобы посмотреть правила игры.''\n'
+    f'Нажми {BUTTON_HELP}, чтобы получить справку по использованию бота.')
+MESSAGE_HELP: str = (  # НЕ НАПИСАЛ ДО КОНЦА!!!!!!
+    'Как играть через бота:\n\n'
+
+    '0. Сбор команды\n\n'
+    'Дорогие друзья! Целью создания данного бота было не сделать игру "Пока '
+    'я сплю" доступной в онлайн, а лишь освободить игроков от необходимости '
+    'иметь при себе ее физический экземпляр. Я всей душой рекомендую играть '
+    'в настолку, только собравшись всем вместе. Лишь в этом случае вы сможете '
+    'по-настоящему ощутить атмосферу этой потрясающей игры!\n\n'
+
+    '1. Создание новой игровой сессии\n\n'
+
+    'Для того, чтобы создать игровую сессию, одному из членов команды '
+    'необходимо использовать команду /create. В ответном сообщении бот '
+    'попросит придумать пароль для игровой сессии из четырех цифр. Его нужно '
+    'отправить как обычное сообщение.\n\n'
+
+    '2. Присоединение к существующей игровой сессии\n\n'
+
+    'Чтобы присоединиться к созданной игровой сессии, остальным членам '
+    'команды необходимо использовать команду /join. В ответном сообщении бот '
+    'попросит ввести пароль. Его нужно отправить как обычное сообщение.'
+    'Пароль должен полностью совпадать с придуманным в пункте 1.\n\n'
+
+    '3. Начало игры..... to be continue..'
+
+    # добавить пункт, если сервер вылетел!
+)
+MESSAGE_JOIN_GAME: str = (
+    'Приветствую, сновидец! Ты готов отправиться с друзьями в новое '
+    'путешествие по миру снов? Отлично! Пожалуйста, пришли мне пароль от '
+    'игры, который придумал капитан команды!')
+MESSAGE_JOIN_GAME_FAILED: str = (
+    'О нет! Приключился неверный пароль! Пожалуйста, уточни у капитана его '
+    'еще раз и пришли мне новую комбинацию цифр!')
+MESSAGE_JOIN_GAME_PASS: str = (
+    'Пароль принят, сновидец! Когда вся команда будет в сборе, капитан '
+    'начнет новую игру, а я тебе сразу же пришлю об этом уведомление! '
+    'Желаю здорово повеселиться! А если вдруг ты решишь проснутся - используй '
+    f'команду {BUTTON_EXIT}.')
+MESSAGE_LEAVE_GROUP_CHAT: str = (
+    'Я могу работать только в личных переписках и вынужден покинуть этот чат!')
+MESSAGE_NEXT_ROUND: str = (
+    'В следующем раунде ты будешь сновидцем, остальные игроки - волшебными '
+    f'созданиями. Когда все будут готовы, нажми {KEYBOARD_START_NEXT_ROUND}.')
+MESSAGE_TEAMMATE: str = (
+    'Проверь список сновидцев ниже: когда вся команда будет в сборе, нажми '
+    f'{BUTTON_BEGIN}, чтобы отправиться в путешествие. Желаю хорошей игры!'
+    '\n\n')
+MESSAGE_PLAYER_MUST_SLEEP: str = (
+    'Теперь закрывай глаза, баю-бай. Игра началась!')
+MESSAGE_PLAYER_ROLE: dict[str, str] = {
+    BUKA: (
+        'В этом раунде ты будешь грозной букой. Обманывай сновидца, сбивай '
+        'его с пути, преврати его сны в кошмары! Важно, чтобы сновидец не '
+        'отгадал ни единого слова!'),
+    FAIRY: (
+        'В этом раунде ты будешь доброй феей. Всячески помогай сновидцу '
+        'пройти по его нелегкому пути. Важно, чтобы сновидец отгадал '
+        'все-все-все слова!'),
+    SANDMAN: (
+        'В этом раунде ты будешь песочным человеком. Поддерживай хрупкий мир '
+        'снов в гармонии. Помогай сновидцу отбиваться от кошмаров, если он не '
+        'справляется, сбивай его с пути, если путь его слишком легок. Важно, '
+        'чтобы сновидец отгадал половину слов!')}
+MESSAGE_ROUND_RESULTS: str = (
+    'Та-да! А вот и утро! Но перед тем как будить нашего спящего '
+    'путешественника, попросите его в мельчайших подробностях рассказать свой '
+    'сон. Пусть это будет абсолютный полет фантазии, полный внезапных '
+    'сумасбродных сюжетных поворотов!\n\n'
+    'Кстати, а вот такие получились результаты раунда:\n'
+    '- слов угадано верно: {correct_answers}\n'
+    '- слов угадано неверно: {incorrect_answers}')
+
+USER_STATE_WANT_JOIN: int = 0
+USER_STATE_WANT_CREATE: int = 1
+USER_STATE_IN_GAME: int = 2
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from datetime import datetime, timedelta
-from dotenv import load_dotenv
 import logging
-import os
 from pathlib import Path
 from random import shuffle
 from re import match
@@ -10,192 +8,40 @@ from telegram.ext import (
     CommandHandler, Dispatcher, Filters, MessageHandler, Updater)
 
 import app_logger
+from app_data import (
+    ALL_DATA, TELEGRAM_BOT_TOKEN,
 
-load_dotenv()
+    BUTTON_ADD_ME_PENALTY, BUTTON_BEGIN, BUTTON_CREATE, BUTTON_CORRECT_ANSWER,
+    BUTTON_EXIT, BUTTON_HELP, BUTTON_INCORRECT_ANSWER, BUTTON_JOIN,
+    BUTTON_RULES, BUTTON_START, BUTTON_START_NEXT_ROUND,
+
+    IMAGE_CARDS, IMAGE_RULES_MEDIA,
+
+    KEYBOARD_EMPTY, KEYBOARD_IN_GAME, KEYBOARD_IN_LOBBY,
+    KEYBOARD_IN_LOBBY_HOST, KEYBOARD_MAIN_MENU, KEYBOARD_START_NEXT_ROUND,
+
+    BUKA, DREAMER, FAIRY, CHARACTERS_CONFIG,
+
+    MESSAGE_CANT_CREATE_OR_JOIN, MESSAGE_CREATE_GAME,
+    MESSAGE_CREATE_GAME_FAILED, MESSAGE_CREATE_GAME_PASS, MESSAGE_GAME_BEGIN,
+    MESSAGE_GREET_1, MESSAGE_GREET_2, MESSAGE_HELP, MESSAGE_JOIN_GAME,
+    MESSAGE_JOIN_GAME_FAILED, MESSAGE_JOIN_GAME_PASS, MESSAGE_LEAVE_GROUP_CHAT,
+    MESSAGE_NEXT_ROUND, MESSAGE_TEAMMATE, MESSAGE_PLAYER_MUST_SLEEP,
+    MESSAGE_PLAYER_ROLE, MESSAGE_ROUND_RESULTS,
+
+    USER_STATE_WANT_JOIN, USER_STATE_WANT_CREATE, USER_STATE_IN_GAME)
+
+from app_settings import (
+    API_TELEGRAM_UPDATE_SEC, PASSWORD_LEN, ROUND_SEC,
+    SHUFFLE_IMAGE_WORDS_COUNT)
+
 logger: logging.Logger = app_logger.get_logger(__name__)
 
-TELEGRAM_BOT_TOKEN: str = os.getenv('TELEGRAM_BOT_TOKEN')
-ALL_DATA: list[str] = [TELEGRAM_BOT_TOKEN]
-
-API_TELEGRAM_UPDATE_SEC: int = 0.5
-
-# Проверить, что все кнопки реально нужны
-# Возможно добавить кнопку, чтобы завершить игру досрочно
-# Вообще надо нарисовать карту возможных развитий событий тыканья кнопок
-BUTTON_ADD_ME_PENALTY: str = '/add_me_penalty'
-BUTTON_BEGIN: str = '/begin'
-BUTTON_CREATE: str = '/create'
-BUTTON_CORRECT_ANSWER: str = '/YES'
-BUTTON_EXIT: str = '/exit'
-BUTTON_HELP: str = '/help'
-BUTTON_INCORRECT_ANSWER: str = '/NO'
-BUTTON_JOIN: str = '/join'
-BUTTON_RULES: str = '/rules'
-BUTTON_START: str = '/start'
-BUTTON_START_NEXT_ROUND: str = '/next_round'
-
-# А карты можно отправлять так:
-# for i in range(0, round) and range(round, len(players))
-IMAGE_CARDS_ORIGINAL_PATH: Path = Path('res/words_original')
-IMAGE_CARDS_ROTATED_PATH: Path = Path('res/words_rotated')
-IMAGE_CARDS: list[Path] = (
-    list(IMAGE_CARDS_ORIGINAL_PATH.iterdir())
-    + list(IMAGE_CARDS_ROTATED_PATH.iterdir()))
-IMAGE_RULES: list[Path] = [
-    Path(f'res/rules/0{i}_правила.jpg') for i in range(8)]
-IMAGE_RULES_MEDIA: list[InputMediaPhoto] = []
-for file_path in IMAGE_RULES:
-    with open(file_path, 'rb') as file:
-        IMAGE_RULES_MEDIA.append(InputMediaPhoto(
-            media=file.read(), caption=Path(file_path).name))
-
-KEYBOARD_EMPTY: list[list[str]] = [
-    ['']]
-KEYBOARD_IN_GAME: list[list[str]] = [
-    [BUTTON_CORRECT_ANSWER, BUTTON_ADD_ME_PENALTY, BUTTON_INCORRECT_ANSWER]]
-KEYBOARD_IN_LOBBY: list[list[str]] = [
-    [BUTTON_RULES, BUTTON_HELP, BUTTON_EXIT]]
-KEYBOARD_IN_LOBBY_HOST: list[list[str]] = [
-    [BUTTON_BEGIN, BUTTON_RULES, BUTTON_HELP, BUTTON_EXIT]]
-KEYBOARD_MAIN_MENU: list[list[str]] = [
-    [BUTTON_CREATE, BUTTON_JOIN, BUTTON_RULES, BUTTON_HELP]]
-KEYBOARD_START_NEXT_ROUND: list[list[str]] = [
-    [BUTTON_START_NEXT_ROUND, BUTTON_EXIT]]
-
-BUKA: str = 'buka'
-DREAMER: str = 'dreamer'
-FAIRY: str = 'fairy'
-SANDMAN: str = 'sandman'
-CHARACTERS_CONFIG: dict[int, dict] = {
-    3:  {BUKA: 1, FAIRY: 1, SANDMAN: 1},
-    4:  {BUKA: 1, FAIRY: 1, SANDMAN: 2},
-    5:  {BUKA: 1, FAIRY: 2, SANDMAN: 2},
-    6:  {BUKA: 2, FAIRY: 3, SANDMAN: 1},
-    7:  {BUKA: 2, FAIRY: 3, SANDMAN: 2},
-    8:  {BUKA: 3, FAIRY: 4, SANDMAN: 1},
-    9:  {BUKA: 3, FAIRY: 4, SANDMAN: 2},
-    10: {BUKA: 4, FAIRY: 5, SANDMAN: 1}}
-
-MESSAGE_CANT_CREATE_OR_JOIN: str = (
-    'Сновидец, я вижу, что ты уже участвуешь в одной из игр. Чтобы создать '
-    'новую или подключиться к другой, тебе сначала нужно выйти из текущей! '
-    f'Для этого воспользуйся командой {BUTTON_EXIT}.')
-MESSAGE_CREATE_GAME: str = (
-    'Приветствую, капитан! Ты готов отправиться со своей командой в новое '
-    'путешествие по миру снов? Отлично! Пожалуйста, пришли мне пять цифр, '
-    'которые будут являться паролем к игре!')
-MESSAGE_CREATE_GAME_FAILED: str = (
-    'Какая удача, капитан! Ты угадал чей-то пароль! А ведь шанс такого '
-    'события составляет менее 1%! Невероятно! Пожалуйста, пришли мне иную '
-    'комбинацию цифр, попробуем еще раз!')
-MESSAGE_CREATE_GAME_PASS: str = (
-    'Пароль принят, капитан! Теперь в точности сообщи его своим друзьям, '
-    f'чтобы они смогли подключиться через команду {BUTTON_JOIN}.''\n\n'
-    f'Когда вся команда будет в сборе, используй команду {BUTTON_BEGIN} для '
-    'начала игры. Желаю здорово повеселиться!\n\n'
-    f'А если вдруг ты решишь проснутся - используй команду {BUTTON_EXIT}. '
-    'Игра при этом не остановится!')
-MESSAGE_GAME_BEGIN: str = (
-    'Твое путешествие начинается через.. 3.. 2.. 1.. Сейчас!')
-MESSAGE_GREET_1: str = (
-    'Добро пожаловать, о чудесный сновидец!\n\n'
-    'Сегодня ты отправляешься в невероятное путешествие по миру снов. Миру, '
-    'где обитают сказочные создания, вершится магия и процветает фантазия. '
-    'Там ты встретишь друзей и врагов, правдолюбов и лжецов, людей искренних '
-    'и лицемерных.\n\n'
-    'Чтобы успешно пройти через любые испытания тебе нужно проявить смелость, '
-    'умение думать на ходу и, конечно же, чувство юмора. Чтобы помочь тебе на '
-    'этом пути я высылаю тебе правила игры. Приятных снов!')
-MESSAGE_GREET_2: str = (
-    f'Нажми {BUTTON_CREATE}, чтобы создать новую игру.''\n'
-    f'Нажми {BUTTON_JOIN}, чтобы присоединиться к игре.''\n'
-    f'Нажми {BUTTON_RULES}, чтобы посмотреть правила игры.''\n'
-    f'Нажми {BUTTON_HELP}, чтобы получить справку по использованию бота.')
-MESSAGE_HELP: str = (  # НЕ НАПИСАЛ ДО КОНЦА!!!!!!
-    'Как играть через бота:\n\n'
-
-    '0. Сбор команды\n\n'
-    'Дорогие друзья! Целью создания данного бота было не сделать игру "Пока '
-    'я сплю" доступной в онлайн, а лишь освободить игроков от необходимости '
-    'иметь при себе ее физический экземпляр. Я всей душой рекомендую играть '
-    'в настолку, только собравшись всем вместе. Лишь в этом случае вы сможете '
-    'по-настоящему ощутить атмосферу этой потрясающей игры!\n\n'
-
-    '1. Создание новой игровой сессии\n\n'
-
-    'Для того, чтобы создать игровую сессию, одному из членов команды '
-    'необходимо использовать команду /create. В ответном сообщении бот '
-    'попросит придумать пароль для игровой сессии из четырех цифр. Его нужно '
-    'отправить как обычное сообщение.\n\n'
-
-    '2. Присоединение к существующей игровой сессии\n\n'
-
-    'Чтобы присоединиться к созданной игровой сессии, остальным членам '
-    'команды необходимо использовать команду /join. В ответном сообщении бот '
-    'попросит ввести пароль. Его нужно отправить как обычное сообщение.'
-    'Пароль должен полностью совпадать с придуманным в пункте 1.\n\n'
-
-    '3. Начало игры..... to be continue..'
-
-    # добавить пункт, если сервер вылетел!
-)
-MESSAGE_JOIN_GAME: str = (
-    'Приветствую, сновидец! Ты готов отправиться с друзьями в новое '
-    'путешествие по миру снов? Отлично! Пожалуйста, пришли мне пароль от '
-    'игры, который придумал капитан команды!')
-MESSAGE_JOIN_GAME_FAILED: str = (
-    'О нет! Приключился неверный пароль! Пожалуйста, уточни у капитана его '
-    'еще раз и пришли мне новую комбинацию цифр!')
-MESSAGE_JOIN_GAME_PASS: str = (
-    'Пароль принят, сновидец! Когда вся команда будет в сборе, капитан '
-    'начнет новую игру, а я тебе сразу же пришлю об этом уведомление! '
-    'Желаю здорово повеселиться! А если вдруг ты решишь проснутся - используй '
-    f'команду {BUTTON_EXIT}.')
-MESSAGE_LEAVE_GROUP_CHAT: str = (
-    'Я могу работать только в личных переписках и вынужден покинуть этот чат!')
-MESSAGE_NEXT_ROUND: str = (
-    'В следующем раунде ты будешь сновидцем, остальные игроки - волшебными '
-    f'созданиями. Когда все будут готовы, нажми {KEYBOARD_START_NEXT_ROUND}.')
-MESSAGE_TEAMMATE: str = (
-    'Проверь список сновидцев ниже: когда вся команда будет в сборе, нажми '
-    f'{BUTTON_BEGIN}, чтобы отправиться в путешествие. Желаю хорошей игры!'
-    '\n\n')
-MESSAGE_PLAYER_MUST_SLEEP: str = (
-    'Теперь закрывай глаза, баю-бай. Игра началась!')
-MESSAGE_PLAYER_ROLE: dict[str, str] = {
-    BUKA: (
-        'В этом раунде ты будешь грозной букой. Обманывай сновидца, сбивай '
-        'его с пути, преврати его сны в кошмары! Важно, чтобы сновидец не '
-        'отгадал ни единого слова!'),
-    FAIRY: (
-        'В этом раунде ты будешь доброй феей. Всячески помогай сновидцу '
-        'пройти по его нелегкому пути. Важно, чтобы сновидец отгадал '
-        'все-все-все слова!'),
-    SANDMAN: (
-        'В этом раунде ты будешь песочным человеком. Поддерживай хрупкий мир '
-        'снов в гармонии. Помогай сновидцу отбиваться от кошмаров, если он не '
-        'справляется, сбивай его с пути, если путь его слишком легок. Важно, '
-        'чтобы сновидец отгадал половину слов!')}
-MESSAGE_ROUND_RESULTS: str = (
-    'Та-да! А вот и утро! Но перед тем как будить нашего спящего '
-    'путешественника, попросите его в мельчайших подробностях рассказать свой '
-    'сон. Пусть это будет абсолютный полет фантазии, полный внезапных '
-    'сумасбродных сюжетных поворотов!\n\n'
-    'Кстати, а вот такие получились результаты раунда:\n'
-    '- слов угадано верно: {correct_answers}\n'
-    '- слов угадано неверно: {incorrect_answers}')
-
-PASSWORD_LEN: int = 5
-
-ROUND_SEC: int = 60 * 2
-
-SHUFFLE_IMAGE_WORDS_COUNT: int = 3
-
-USER_STATE_WANT_JOIN: int = 0
-USER_STATE_WANT_CREATE: int = 1
-USER_STATE_IN_GAME: int = 2
-
 bot: Bot = Bot(token=TELEGRAM_BOT_TOKEN)
+
+active_games: dict[str, dict[str, any]] = {}
+users_passwords: dict[int, int] = {}
+users_states: dict[int, int] = {}
 
 # Может быть сделать ачивки:
 #   - яркие сны: угадал больше всего слов
@@ -206,18 +52,7 @@ bot: Bot = Bot(token=TELEGRAM_BOT_TOKEN)
 #   - лицемерище: заработал больше всего очков как песочный человек
 #   - кайфоломщик: получил больше всего пенальти
 
-active_games: dict[str, dict[str, any]] = {}
-# А вот непонятно - тут int или не int будет приходить, скорее всего str
-users_passwords: dict[int, int] = {}
-users_states: dict[int, int] = {}
-
-# Admin может добавлять или отнимать баллы к правильным в конце тура!
-# И выдать штраф!
-
-# Если карты кончились (чего быть не может) - игра заканчивается.
-
 # А если будет дабл-клик по кнопке?
-
 # Сделать проверку, что игрок не в игре, чтобы ему кнопки не сбить!
 
 # Рассмотреть 2 ситуации
@@ -225,6 +60,7 @@ users_states: dict[int, int] = {}
 #   - когда уходит игрок, который не сыграл
 
 
+# Только один параметр!
 def check_env(data: list) -> None:
     """Checks env data."""
     if not all(data):
